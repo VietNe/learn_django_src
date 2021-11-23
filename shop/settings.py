04172017f76ab57f9c148f05547d10630ae31243
nqviet.dev <nqviet.dev@gmail.com>
2021-11-23 11:31:59 +0700

    add template for website

--- shop/settings.py
+++ shop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'cart',
     'order',
     'user',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,9 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'user.CustomerUser'
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
